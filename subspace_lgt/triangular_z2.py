aface481ff5eb10317b7fdb7f9385cf427b8756d
Yutaro Iiyama <yutaro.iiyama@cern.ch>
2024-10-08 03:17:46 +0900

    Full rework for triangular lattice

--- subspace_lgt/triangular_z2.py
+++ subspace_lgt/triangular_z2.py
@@ -1,135 +1,120 @@
 """Triangular lattice for Z2 pure-gauge Hamiltonian."""
-from numbers import Integral
-import numpy as np
+from itertools import count
 from qiskit.quantum_info import SparsePauliOp
+import rustworkx as rx
 
 
 class TriangularZ2Lattice:
-    """Lattice definition and conversion of operator placement to Pauli strings."""
+    """Triangular lattice for pure-Z2 gauge theory."""
     def __init__(self, configuration):
-        self._configuration = configuration
+        # Sanitize the configuration string
+        config_rows = configuration.split('\n')
+        first_row = 0
+        while not config_rows[first_row].strip():
+            first_row += 1
+        config_rows = config_rows[first_row:]
+        last_row = len(config_rows)
+        while not config_rows[last_row - 1].strip():
+            last_row -= 1
+        config_rows = config_rows[:last_row]
+        first_column = 0
+        while all(not row[first_column] for row in config_rows):
+            first_column += 1
+        config_rows = [row[first_column:] for row in config_rows]
+        last_column = max(len(row) for row in config_rows)
+        while all(len(row) < last_column or not row[last_column - 1] for row in config_rows):
+            last_column -= 1
+        config_rows = [row[:last_column] for row in config_rows]
+        config_rows = [row + (' ' * (last_column - len(row))) for row in config_rows]
 
-        plaquette_row_starts = [0]
-        link_row_starts = [0]
-        site_row_starts = [0]
-        for row_conf in configuration:
-            plaquette_row_starts.append(plaquette_row_starts[-1] + len(row_conf))
-            num_horizontal_links = sum(1 for x in row_conf if x == 'v')
-            link_row_starts.append(link_row_starts[-1] + num_horizontal_links)
-            link_row_starts.append(link_row_starts[-1] + len(row_conf) + 1)
-            site_row_starts.append(site_row_starts[-1] + num_horizontal_links + 1)
+        for upper, lower in zip(config_rows[:-1], config_rows[1:]):
+            if any(u == '*' and l == '*' for u, l in zip(upper, lower)):
+                raise ValueError('Invalid lattice configuration')
 
-        num_horizontal_links = sum(1 for x in configuration[-1] if x == '^')
-        link_row_starts.append(link_row_starts[-1] + num_horizontal_links)
-        site_row_starts.append(site_row_starts[-1] + num_horizontal_links + 1)
-        self._plaquette_row_starts = np.array(plaquette_row_starts, dtype=int)
-        self._link_row_starts = np.array(link_row_starts, dtype=int)
-        self._site_row_starts = np.array(site_row_starts, dtype=int)
+        self.graph = rx.PyGraph()
+        self.graph.add_nodes_from(range(configuration.count('*')))
+
+        node_id_gen = iter(self.graph.node_indices())
+        node_ids = []
+        for row in config_rows:
+            node_ids.append([next(node_id_gen) if char == '*' else None for char in row])
+
+        edge_id_gen = iter(count())
+        for upper, lower in zip(node_ids[:-1], node_ids[1:]):
+            for ipos, left in enumerate(upper[:-2]):
+                if left is not None and (right := upper[ipos + 2]) is not None:
+                    self.graph.add_edge(left, right, next(edge_id_gen))
+            for ipos, top in enumerate(upper):
+                if top is None:
+                    continue
+                if ipos > 0 and (bottom := lower[ipos - 1]) is not None:
+                    self.graph.add_edge(top, bottom, next(edge_id_gen))
+                if ipos < len(lower) - 1 and (bottom := lower[ipos + 1]) is not None:
+                    self.graph.add_edge(top, bottom, next(edge_id_gen))
+
+        for ipos, left in enumerate(node_ids[-1][:-2]):
+            if left is not None and (right := node_ids[-1][ipos + 2]) is not None:
+                self.graph.add_edge(left, right, next(edge_id_gen))
+
+        self.dual_graph = rx.PyGraph()
+        self.dual_graph.add_nodes_from([('edge', idx) for idx in self.graph.edge_indices()])
+
+        plaq_id_gen = iter(count())
+        for upper, lower in zip(node_ids[:-1], node_ids[1:]):
+            for ipos, top in enumerate(upper[:-1]):
+                endpoints = None
+                if (top is None and ipos > 0 and (left := upper[ipos - 1]) is not None
+                        and (right := upper[ipos + 1]) is not None
+                        and (bottom := lower[ipos]) is not None):
+                    endpoints = [(left, right), (right, bottom), (bottom, left)]
+
+                if (top is not None and ipos > 0 and (left := lower[ipos - 1]) is not None
+                        and (right := lower[ipos + 1]) is not None):
+                    endpoints = [(left, right), (right, top), (top, left)]
+
+                if not endpoints:
+                    continue
+
+                plaq_node_id = self.dual_graph.add_node(('plaq', next(plaq_id_gen)))
+                for n1, n2 in endpoints:
+                    self.dual_graph.add_edge(
+                        list(self.graph.edge_indices_from_endpoints(n1, n2))[0],
+                        plaq_node_id,
+                        None
+                    )
 
     @property
     def num_plaquettes(self):
-        return self._plaquette_row_starts[-1]
+        return len(self.dual_graph.filter_nodes(lambda data: data[0] == 'plaq'))
 
     @property
     def num_links(self):
-        return self._link_row_starts[-1]
+        return self.graph.num_edges()
 
     @property
     def num_sites(self):
-        return self._site_row_starts[-1]
+        return self.graph.num_nodes()
 
-    def plaquette_id(self, coordinate):
-        row, column = coordinate
-        if column >= self._plaquette_row_starts[row + 1]:
-            raise ValueError(f'Invalid plaquette column number {column} for row {row}')
-        return self._plaquette_row_starts[row] + column
+    def plaquette_links(self, plaq_id):
+        plaq_node = list(self.dual_graph.filter_nodes(lambda data: data == ('plaq', plaq_id)))[0]
+        return list(self.dual_graph.neighbors(plaq_node))
 
-    def plaquette_coordinate(self, plaquette_id):
-        row = np.searchsorted(self._plaquette_row_starts, plaquette_id, side='right') - 1
-        return (row, plaquette_id - self._plaquette_row_starts[row])
+    def site_links(self, site_id):
+        return list(self.graph.incident_edges(site_id))
 
-    def plaquette_type(self, id_or_coordinate):
-        if isinstance(id_or_coordinate, int):
-            row, col = self.plaquette_coordinate(id_or_coordinate)
-        else:
-            row, col = id_or_coordinate
-
-        return self._configuration[row][col]
-
-    def plaquette_links(self, id_or_coordinate):
-        if isinstance(id_or_coordinate, Integral):
-            row, col = self.plaquette_coordinate(id_or_coordinate)
-        else:
-            row, col = id_or_coordinate
-
-        if self.plaquette_type((row, col)) == 'v':
-            link_coordinates = [
-                (row * 2, col // 2),
-                (row * 2 + 1, col),
-                (row * 2 + 1, col + 1)
-            ]
-        else:
-            link_coordinates = [
-                (row * 2 + 1, col),
-                (row * 2 + 1, col + 1),
-                (row * 2 + 2, col // 2)
-            ]
-        return [self.link_id(coord) for coord in link_coordinates]
-
-    def link_id(self, coordinate):
-        row, column = coordinate
-        if column >= self._link_row_starts[row + 1]:
-            raise ValueError(f'Invalid column number {column} for row {row}')
-        return self._link_row_starts[row] + column
-
-    def link_coordinate(self, link_id):
-        row = np.searchsorted(self._link_row_starts, link_id, side='right') - 1
-        return (row, link_id - self._link_row_starts[row])
-
-    def link_coordinates(self):
-        return sum(([(row, col) for col in range(row_size)]
-                    for row, row_size in enumerate(np.diff(self._link_row_starts))),
-                   [])
-
-    def site_id(self, coordinate):
-        row, column = coordinate
-        if column >= self._site_row_starts[row + 1]:
-            raise ValueError(f'Invalid site column number {column} for row {row}')
-        return self._site_row_starts[row] + column
-
-    def site_coordinate(self, site_id):
-        row = np.searchsorted(self._site_row_starts, site_id, side='right') - 1
-        return (row, site_id - self._site_row_starts[row])
-
-    def site_links(self, id_or_coordinate):
-        if isinstance(id_or_coordinate, Integral):
-            row, col = self.site_coordinate(id_or_coordinate)
-        else:
-            row, col = id_or_coordinate
-
-        link_coordinates = []
-        return [self.link_id(coord) for coord in link_coordinates]
-
-    def to_pauli(self, placements):
-        """Form the Pauli string corresponding to the given placement of operators in the
-        configuration.
+    def to_pauli(self, link_ops):
+        """Form the Pauli string corresponding to the given link operators.
 
         Args:
-            placements: {(row, col): operator} or {link_id: operator}
+            link_ops: {link_id: operator}
             configuration: Lattice configuration.
 
         Returns:
             Pauli string corresponding to the operator placement.
         """
         link_paulis = []
-        for id_or_coordinate, op in placements.items():
-            if isinstance(id_or_coordinate, Integral):
-                row, col = self.link_coordinate(id_or_coordinate)
-            else:
-                row, col = id_or_coordinate
-            if col >= self._link_row_starts[row + 1]:
-                raise ValueError(f'Invalid column number {col} for row {row}')
-            link_paulis.append((self.link_id((row, col)), op.upper()))
+        for link_id, op in link_ops.items():
+            link_paulis.append((link_id, op.upper()))
 
         link_paulis = sorted(link_paulis, key=lambda x: x[0])
         pauli = ''
@@ -140,7 +125,7 @@ class TriangularZ2Lattice:
         return pauli
 
     def make_hamiltonian(self, plaquette_energy):
-        link_terms = [self.to_pauli({coord: 'Z'}) for coord in self.link_coordinates()]
+        link_terms = [self.to_pauli({link_id: 'Z'}) for link_id in self.graph.edge_indices()]
         plaquette_terms = []
         for plid in range(self.num_plaquettes):
             plaquette_terms.append(self.to_pauli({lid: 'X' for lid in self.plaquette_links(plid)}))
