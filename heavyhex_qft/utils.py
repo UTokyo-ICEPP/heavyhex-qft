d5deca975c77c0a1bda12df31687ec0410d6c3ed
Yutaro Iiyama <yutaro.iiyama@cern.ch>
2024-10-09 02:11:52 +0900

    Forgot to commit utils

---
+++ heavyhex_qft/utils.py
@@ -0,0 +1,10 @@
+"""Miscellaneous utility functions."""
+import numpy as np
+from qiskit.quantum_info import SparsePauliOp
+
+
+def subspace_projection(op: SparsePauliOp, indices: np.ndarray) -> np.ndarray:
+    if len(indices.shape) != 1:
+        raise ValueError('Subspace projection indices must be a 1D array')
+
+    return op.to_matrix(sparse=True)[indices[:, None], indices[None, :]].todense()
