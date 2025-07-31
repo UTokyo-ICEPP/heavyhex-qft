21217106cd339716b977813984d323fce6f3b7d5
Yutaro Iiyama <yutaro.iiyama@cern.ch>
2025-08-01 00:04:20 +0900

    validated general projection

--- heavyhex_qft/subspace.py
+++ heavyhex_qft/subspace.py
@@ -1,18 +1,20 @@
 """Subspace projection routines."""
 from functools import partial
 import numpy as np
-from scipy.sparse import coo_array
+# from scipy.sparse import coo_array
 import jax
 import jax.numpy as jnp
+from jax.experimental.sparse import BCOO, bcoo_dot_general
 from qiskit.quantum_info import SparsePauliOp
+from .linalg import lobpcg_standard
 
-PAULI_BIT_MAP = np.array([
+PAULI_BIT_MAP = jnp.array([
     [0, 1],
     [1, 0],
     [1, 0],
     [0, 1]
-])
-PAULI_COEFFS = np.array([
+], dtype=np.uint8)
+PAULI_COEFFS = jnp.array([
     [1., 1.],
     [1., 1.],
     [1.j, -1.j],
@@ -22,6 +24,7 @@ PAULI_INDEX = {c: i for i, c in enumerate('IXYZ')}
 
 
 def subspace_projection(op: SparsePauliOp, indices: np.ndarray) -> np.ndarray:
+    indices = np.asarray(indices)
     if len(indices.shape) == 1:
         if op.num_qubits < 32:
             return np.asarray(
@@ -29,11 +32,18 @@ def subspace_projection(op: SparsePauliOp, indices: np.ndarray) -> np.ndarray:
             )
         indices = (indices[:, None] >> np.arange(op.num_qubits)[None, ::-1]) % 2
 
-    col_bitstrings = jnp.array(indices)
-    paulistrings = jnp.array([[PAULI_INDEX[c] for c in p.tolabel()] for p in op.paulis])
-    row_bitstrings, coeffs = map_bitstrings_by_paulis(col_bitstrings, paulistrings)
-
-    # filter out row_bitstrings with indices and construct a coo array
+    proj_dim = indices.shape[0]
+    indices = jnp.array(indices)
+    paulistrings = jnp.array([[PAULI_INDEX[c] for c in p.to_label()] for p in op.paulis])
+    mapped_bitstrings, pauli_mes = map_bitstrings_by_paulis(indices, paulistrings)
+    rows = bitstring_positions(mapped_bitstrings.reshape((-1, op.num_qubits)), indices)
+    coeffs = (pauli_mes * op.coeffs[:, None]).reshape(-1)
+    in_subspace = jnp.not_equal(rows, -1)
+    data = coeffs[in_subspace]
+    rows = rows[in_subspace]
+    cols = jnp.tile(jnp.arange(proj_dim), len(op.paulis))[in_subspace]
+    indices = jnp.stack([rows, cols], axis=1)
+    return BCOO((data, indices), shape=(proj_dim, proj_dim))
 
 
 def map_bitstring_by_pauli(bitstring: np.ndarray, paulistring: np.ndarray, npmod=np):
@@ -48,15 +58,21 @@ map_bitstrings_by_paulis = jax.jit(jax.vmap(map_bitstrings_by_pauli, in_axes=(No
 
 
 @jax.jit
-def bitstring_isin(bitstring: jax.Array, pool: jax.Array):
-    def fun(carry, pos):
-        low, high = carry
-        sublow = jnp.searchsorted(bitstring[pos], pool[low:high, pos], 'left')
-        subhigh = jnp.searchsorted(bitstring[pos], pool[low:high, pos], 'right')
-        high = low + subhigh
-        low += sublow
-        return (low, high), jnp.greater(high, low)
+def bitstring_position(bitstring: jax.Array, pool: jax.Array):
+    matches = jnp.all(jnp.equal(bitstring[None, :], pool), axis=1)
+    idx = jnp.argmax(matches)
+    return jax.lax.select(jnp.equal(idx, 0), jax.lax.select(matches[0], 0, -1), idx)
 
-    num_qubits = bitstring.shape[0]
-    (low, high), matches = jax.lax.scan(fun, (0, pool.shape[0]), jnp.arange(num_qubits))
-    return jax.lax.cond(matches[-1], low, -1)
+
+bitstring_positions = jax.jit(jax.vmap(bitstring_position, in_axes=(0, None)))
+
+
+def multiply_bcoo(x, bcoo):
+    return bcoo_dot_general(-bcoo, x, dimension_numbers=(([1], [0]), ([], [])))
+
+
+def subspace_ground_state(mat):
+    xmat = jnp.ones((mat.shape[0], 1), dtype=np.complex128)
+    # pylint: disable-next=unbalanced-tuple-unpacking
+    jeigvals, jeigvecs, _ = lobpcg_standard(multiply_bcoo, xmat, args=(mat,))
+    return float(-jeigvals[0]), np.array(jeigvecs[:, 0])
